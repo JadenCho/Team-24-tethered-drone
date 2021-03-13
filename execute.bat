443da11a5116de703f43895c6237564c53d535f2
henrybojanowski <henryboj@bu.edu>
2021-03-12 21:08:32 -0500

    added .bat file for running three commands and made command for opening mission planner

---
+++ execute.bat
@@ -0,0 +1,5 @@
+start cmd.exe /k "node filewriter.js"
+start cmd.exe /k "python mainx.py"  
+start cmd.exe /k "cd C:\FlyJus\dev\ardupilot\build\sitl\bin && arduplane.exe -Mxplane -s1 --uartA tcp:0" 
+start cmd.exe /k "cd C:\Program Files (x86)\Mission Planner && missionplanner.exe" 
+
